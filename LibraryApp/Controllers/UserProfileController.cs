359c982a86ff2b4a675533cbdf0dcda5e1c5044d
NazarTretiakov <nazartv2005@gmail.com>
2024-12-08 11:31:44 +0100

    Added controller UserProfileController.cs and view Posts.cshtml in that controller.

---
+++ LibraryApp/Controllers/UserProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.UI.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        [Route("/user-profile")]
+        public IActionResult Index()
+        {
+            return RedirectToAction("Posts");
+        }
+
+        [Route("/user-profile/posts")]
+        public IActionResult Posts()
+        {
+            return View();
+        }
+    }
+}
--- LibraryApp/obj/Debug/net8.0/LibraryApp.UI.AssemblyInfo.cs
+++ LibraryApp/obj/Debug/net8.0/LibraryApp.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2127dff11be0d2d3341f8e2a6177564231f9a2c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a201253b35b2a5381e9a534353d7ea9eec422169")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
